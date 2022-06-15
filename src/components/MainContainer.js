7ed7b0424d437f938bf2473a92b4d70e71695258
chrisrandall <chris.f.randall@gmail.com>
2022-06-15 10:40:59 -0600

    almost done

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,44 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [stocks, setStocks] = useState([])
+  const [portfolio, setPortfolio] = useState([])
+  const [sortBy, setSortBy] = useState("Alphabetically");
+  const [filterBy, setFilterBy] = useState("Tech");
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+      .then((r) => r.json())
+      .then(setStocks);
+  }, []);
+
+  function handleAddStock(stockToAdd){
+    const stockInPortfolio = portfolio.find(
+      (stock) => stock.id === stockToAdd.id
+    )
+    if (!stockInPortfolio) {
+      setPortfolio([...portfolio, stockToAdd]);
+    }
+  }
+
+  function handleDeleteStock(stockToRemove){
+    setPortfolio((portfolio) =>
+      portfolio.filter((stock) => stock.id !== stockToRemove.id)
+    )
+  }
+
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={stocks} onAddStock={handleAddStock}/>
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer stocks={portfolio} onDeleteStock={handleDeleteStock}/>
         </div>
       </div>
     </div>
--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,12 +1,18 @@
 import React from "react";
 
-function Stock() {
+function Stock({stock, onStockClick}) {
+  const { ticker, name, type, price } = stock;
+
+  function handleClick() {
+    onStockClick(stock)
+  }
+
   return (
     <div>
-      <div className="card">
+      <div className="card" onClick={handleClick}>
         <div className="card-body">
-          <h5 className="card-title">{"Compant Name"}</h5>
-          <p className="card-text">{"Stock Price"}</p>
+          <h5 className="card-title">{name} : {ticker} : {type} </h5>
+          <p className="card-text">{price}</p>
         </div>
       </div>
     </div>
